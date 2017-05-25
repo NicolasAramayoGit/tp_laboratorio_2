d4e99a76a7f2ccd25a56a2777243fca884d55219
Nicolas Aramayo <aramayo.fabian@gmail.com>
2017-05-25 14:32:44 -0300

    AGREGO EL TRABAJO PRÁCTICO NÚMERO 3
    Todavía falta escribir código...

---
+++ TP3/Aramayo.Nicolas.2A.TP3/ClasesAbstractas/Persona.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAbstractas
+{
+    public abstract class Persona
+    {
+        private string _apellido;
+        private int _dni;
+        private ENacionalidad _nacionalidad;
+        private string _nombre;
+
+        public enum ENacionalidad
+        {
+
+        }
+
+
+        public string Apellido
+        {
+            get { return this._apellido; }
+            set { this._apellido = value; }
+        }
+
+        public int DNI
+        {
+            get { return this._dni; }
+            set { this._dni = value; }
+        }
+
+        public ENacionalidad Nacionalidad
+        {
+            get { return this._nacionalidad; }
+            set { this._nacionalidad = value; }
+        }
+
+        public string Nombre
+        {
+            get { return this._nombre; }
+            set { this._nombre = value; }
+        }
+
+        //public string StringToDni
+        //{
+        //    set { this._dni = Validar(); }
+        //}
+
+        public Persona()
+        { }
+
+        public Persona(string nombre,string apellido,ENacionalidad nacionalidad)
+        {
+            this.Apellido = apellido;
+            this.Nombre = nombre;
+            this.Nacionalidad = nacionalidad;
+        }
+
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad):this(nombre,apellido,nacionalidad)
+        {
+            this.DNI = dni;
+        }
+
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad):this(nombre,apellido,nacionalidad)
+        {
+
+        }
+    }
+}
